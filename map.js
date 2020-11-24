b25a7d746ea94d22539b05ff499efa2310456f66
jykkim111 <jksmoove14@gmail.com>
2020-11-24 12:03:26 +0900

    Added new map mode

--- map.js
+++ map.js
@@ -1,14 +1,3 @@
-//let input = document.getElementById('input')
-//input.addEventListener('change', function() {
-//    readXlsxFile(input.files[0]).then((rows) => {
-//        console.log(rows);
-//    })
-//})
-//
-
-
-//let data = JSON.parse('data.json');
-
 function excelExport(event) {
     excelExportCommon(event, handleExcelDataJson);
 }
@@ -22,33 +11,164 @@ function excelExportCommon(event, callback) {
         var sheetNameList = wb.SheetNames; // 시트 이름 목록 가져오기 
         var firstSheetName = sheetNameList[1]; // 첫번째 시트명
         var firstSheet = wb.Sheets[firstSheetName]; // 첫번째 시트 
-        callback(firstSheet);
+
+        let data_string = JSON.stringify(XLSX.utils.sheet_to_json(firstSheet));
+        let data_json = JSON.parse(data_string);
+        callback(data_json);
     };
     reader.readAsBinaryString(input.files[0]);
 }
 
-function handleExcelDataJson(sheet) {
-    let data_string = JSON.stringify(XLSX.utils.sheet_to_json(sheet));
-    let data_json = JSON.parse(data_string);
-
+function handleExcelDataJson(data) {
+    //=========================
+    // Draw Map
+    // input: 
+    //      선택된 자치구, 기간에 해당하는 data
+    //      json data: [{"대여소 번호", "보관소(대여소)명", "자치구", "위도", "경도", "대여량"} , {} ...]
+    //=========================
     let gangnam_bike_stations = [];
-    for (let i = 0; i < data_json.length; i++) {
-        if (data_json[i]['자치구'] == '강남구') {
-            gangnam_bike_stations.push(data_json[i]);
-        }
+    for (let i = 0; i < data.length; i++) {
+        //if (data[i]['자치구'] == '강남구') {
+        gangnam_bike_stations.push(data[i]);
+        //}
     }
 
+    console.log(data)
+
     gangnam_bike_stations.forEach(function(d) {
-        var marker = L.marker([d['위도'], d['경도']]).addTo(mymap);
+
+        var circle = L.circle([d['위도'], d['경도']], {
+                color: '#f03',
+                fillColor: '#f03',
+                weight: 0,
+                fillOpacity: 0.8,
+                radius: 50,
+                className: d['대여소'].toString()
+            })
+            .bindTooltip("[" + d['대여소'] + "] " + d['보관소(대여소)명'], {
+                permanent: false,
+                direction: 'right'
+            })
+            .addTo(mymap)
+            .on('mouseover', (event) => {
+                event.target.setStyle({
+                    weight: 3
+                });
+            })
+            .on('mouseout', (event) => {
+                event.target.setStyle({
+                    weight: 0
+                });
+            })
+            .on('mouseup', (event) => {
+                if (event.target.options.color == 'green') {
+                    event.target.setStyle({
+                        color: 'red',
+                        fillColor: 'red',
+                        weight: 0
+                    });
+                } else {
+                    chosenStationNum = event.target.options.className;
+                    console.log(chosenStationNum);
+                    event.target.setStyle({
+                        color: 'green',
+                        fillColor: 'green',
+                        weight: 3
+                    });
+                }
+            });
+
     })
-
-
 
     //$("#displayExcelJson").html(JSON.stringify(XLSX.utils.sheet_to_json(sheet)));
 }
 
+function updateMap() {
+    if (opt.options[opt.selectedIndex].value == "normal") {
+        mymap.eachLayer(function(layer) {
+            let station_num = layer.options.className;
+            //console.log(station_num);
 
-let mymap = L.map('mapid').setView([37.48, 127.05], 13);
+            if (station_num == undefined || station_num == "") {
+                return;
+            } else {
+
+                layer.setStyle({
+                    color: "red",
+                    fillColor: "red",
+                    fillOpacity: 0.8
+                });
+
+            }
+        });
+    }
+
+    if (opt.options[opt.selectedIndex].value == "flow") {
+
+        let updated_color = "#f03";
+
+        mymap.eachLayer(function(layer) {
+            let station_num = layer.options.className;
+            //console.log(station_num);
+
+            if (station_num == undefined || station_num == "") {
+                return;
+            } else {
+                let numLength = station_num.length;
+                if (numLength != 5) {
+                    let zeros = 5 - numLength;
+                    for (let i = 0; i < zeros; i++) {
+                        station_num = "0".concat(station_num);
+                    }
+                }
+                let rented = 0;
+                let returned = 0;
+                if (aggregatedDataForMap[station_num] == undefined) {
+                    return;
+                } else {
+                    rented = aggregatedDataForMap[station_num].rented;
+                    returned = aggregatedDataForMap[station_num].returned;
+                }
+
+
+                let color_code = returned / rented;
+
+                if (color_code < 1) {
+                    updated_color = "red";
+                } else if (color_code > 1.2) {
+                    updated_color = "blue";
+                } else {
+                    updated_color = "green";
+                }
+            }
+
+
+            layer.setStyle({
+                color: updated_color,
+                fillColor: updated_color,
+                weight: 0,
+                fillOpacity: 0.8,
+            });
+
+
+
+
+            //console.log(aggregatedDataForMap["02386"]);
+
+
+        });
+
+    }
+    /*
+    mymap.eachLayer(function(layer) {
+        console.log(layer);
+
+    })
+    */
+}
+
+
+let mymap = L.map('map').setView([37.48, 127.05], 13);
 
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamtzbW9vdmUxNCIsImEiOiJja2hqNzk2ODkwajBxMnNzZDRicjUzeDVnIn0.W6JG8IbwREpA3HRD0T8-7g', {
@@ -59,3 +179,5 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1,
     accessToken: 'your.mapbox.access.token'
 }).addTo(mymap);
+
+let opt = document.querySelector('#map_options');
---
+++ overview.js
@@ -0,0 +1,270 @@
+let data;
+let aggregatedDataByTime;
+let aggregatedDataByRentedStation;
+let aggregatedDataByReturnedStation;
+let inflowByHour;
+let outflowByHour;
+let aggregateTimePerSeconds = 600; // 10분 단위로 모음
+let lineChartXDomain;
+
+/* 
+  startTime, endTime: 선택된 시간 구간
+  1970년 1월 1일 이후의 밀리 초 수로 저장됨 (integer)
+  Date.parse로 대여일시, 반납일시 parse
+
+  aggregatedDataForMap: 각 대여소별로 선택된 시간 범위 내에서 
+  rent된 자전거 대수와 return된 자전거 대수 저장
+  대여소 번호를 key로 가지고, value는 rented와 returned를 저장한 object
+
+  aggregatedDataForMap = {
+    "10000": {
+      rented: 100,
+      returned: 102
+    },
+    "10002": {
+      rented: 201,
+      returned: 188
+    }, 
+    ...
+  }
+
+  brushing으로 선택 시간 갱신할 때마다 
+  aggregatedDataForMap, startTime, endTime 자동 갱신
+*/
+//let aggregatedDataForMap;
+let startTime, endTime;
+
+const lineChartWidth = 1400;
+const lineChartHeight = 150;
+const flowChartWidth = 800;
+const flowChartHeight = 500;
+const margin = 40;
+
+const brush = d3
+    .brushX()
+    .extent([
+        [0, 0],
+        [lineChartWidth, lineChartHeight],
+    ])
+    .on("end", brushed);
+
+const lineChart = d3
+    .select("#linechart")
+    .attr("width", lineChartWidth + margin * 2)
+    .attr("height", lineChartHeight + margin * 2)
+    .append("svg")
+    .attr("width", lineChartWidth + margin * 2)
+    .attr("height", lineChartHeight + margin * 2)
+    .append("g")
+    .attr("transform", `translate(${margin}, ${margin})`);
+
+let lineChartTooltip = d3
+    .select("body")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+var clip = lineChart
+    .append("defs")
+    .append("svg:clipPath")
+    .attr("id", "clip")
+    .append("svg:rect")
+    .attr("width", lineChartWidth)
+    .attr("height", lineChartHeight)
+    .attr("x", 0)
+    .attr("y", 0);
+
+let lineChartX, lineChartY;
+
+const flowChart = d3
+    .select("#flowchart")
+    .attr("width", flowChartWidth)
+    .attr("height", flowChartHeight)
+    .append("svg")
+    .attr("width", flowChartWidth)
+    .attr("height", flowChartHeight)
+    .append("g")
+    .attr("transform", `translate(${margin}, ${margin})`);
+
+
+let flowChartX, flowChartY;
+
+
+
+main();
+
+async function main() {
+    data = await d3.csv("data.csv");
+    lineChartXDomain = d3.extent(data, (d) => Date.parse(d["대여일시"]));
+    [startTime, endTime] = lineChartXDomain;
+    initAggregatedData();
+    initLineChartAxes();
+    initLineChart();
+}
+
+function initAggregatedData() {
+    aggregatedDataByTime = d3
+        .nest()
+        .key(
+            (d) =>
+            parseInt(
+                Date.parse(d["대여일시"]) / (aggregateTimePerSeconds * 1000)
+            ) *
+            (aggregateTimePerSeconds * 1000)
+        )
+        .sortKeys(d3.ascending)
+        .rollup((v) => v.length)
+        .entries(data);
+
+    aggregatedDataByRentedStation = d3
+        .nest()
+        .key((d) => d["대여 대여소번호"])
+        .rollup((v) => v.map((d) => Date.parse(d["대여일시"])))
+        .entries(data);
+
+    aggregatedDataByReturnedStation = d3
+        .nest()
+        .key((d) => d["반납대여소번호"])
+        .rollup((v) => v.map((d) => Date.parse(d["반납일시"])))
+        .entries(data);
+
+    aggregatedDataForMap = {};
+    aggregatedDataByReturnedStation.forEach((v) => {
+        aggregatedDataForMap[v.key] = {
+            rented: 0,
+            returned: 0,
+        };
+    });
+    aggregatedDataByRentedStation.forEach((v) => {
+        aggregatedDataForMap[v.key] = {
+            rented: 0,
+            returned: 0,
+        };
+    });
+}
+
+function aggregateDataForMap() {
+    aggregatedDataByRentedStation.forEach((v) => {
+        aggregatedDataForMap[v.key].rented = v.value.filter(
+            (date) => date >= startTime && date <= endTime
+        ).length;
+    });
+    aggregatedDataByReturnedStation.forEach((v) => {
+        aggregatedDataForMap[v.key].returned = v.value.filter(
+            (date) => date >= startTime && date <= endTime
+        ).length;
+    });
+}
+
+function initLineChartAxes() {
+    lineChartX = d3
+        .scaleTime()
+        .domain(lineChartXDomain)
+        .range([0, lineChartWidth]);
+
+    lineChartY = d3
+        .scaleLinear()
+        .domain(d3.extent(aggregatedDataByTime, (d) => +d.value))
+        .range([lineChartHeight, 0]);
+}
+
+function initLineChart() {
+    lineChart
+        .append("g")
+        .attr("id", "linechart_x")
+        .attr("transform", `translate(0, ${lineChartHeight})`)
+        .call(d3.axisBottom(lineChartX));
+
+    lineChart
+        .append("g")
+        .attr("id", "linechart_y")
+        .call(d3.axisLeft(lineChartY));
+
+    lineChart
+        .append("g")
+        .attr("width", lineChartWidth)
+        .attr("height", lineChartHeight)
+        .attr("id", "linechart_data")
+        .attr("clip-path", "url(#clip)")
+        .selectAll("path")
+        .data([aggregatedDataByTime])
+        .enter()
+        .append("path")
+        .attr("fill", "none")
+        .attr("stroke", "#808080")
+        .attr("d", (data) =>
+            d3
+            .line()
+            .x((d) => lineChartX(+d.key))
+            .y((d) => lineChartY(+d.value))(data)
+        );
+    lineChart
+        .on("dblclick", (event) => {
+            [startTime, endTime] = lineChartXDomain;
+            aggregateDataForMap();
+            updateMap();
+            lineChartX.domain(lineChartXDomain);
+            lineChart
+                .select("#linechart_x")
+                .transition()
+                .duration(1000)
+                .call(d3.axisBottom(lineChartX));
+            lineChart
+                .select("#linechart_data")
+                .select("path")
+                .transition()
+                .duration(1000)
+                .attr(
+                    "d",
+                    d3
+                    .line()
+                    .x((d) => lineChartX(+d.key))
+                    .y((d) => lineChartY(+d.value))(aggregatedDataByTime)
+                );
+        })
+        .on("mouseover", function(event, d) {
+            let pageX = event.pageX;
+            let pageY = event.pageY;
+            lineChartTooltip.transition().style("opacity", 0.9);
+            lineChartTooltip
+                .html(d3.timeFormat("%x %X")(lineChartX.invert(pageX)))
+                .style("left", pageX + "px")
+                .style("top", pageY - 28 + "px");
+        })
+        .on("mouseout", (d) => {
+            lineChartTooltip.transition().duration(200).style("opacity", 0);
+        });
+
+    lineChart.append("g").attr("class", "brush").call(brush);
+}
+
+
+function brushed({ selection }) {
+    if (!selection) return;
+    [startTime, endTime] = selection.map((v) =>
+        Date.parse(lineChartX.invert(v))
+    );
+
+    lineChartX.domain([startTime, endTime]);
+    lineChart.select(".brush").call(brush.move, null);
+
+    lineChart
+        .select("#linechart_x")
+        .transition()
+        .duration(1000)
+        .call(d3.axisBottom(lineChartX));
+    lineChart
+        .select("#linechart_data")
+        .select("path")
+        .transition()
+        .duration(1000)
+        .attr(
+            "d",
+            d3
+            .line()
+            .x((d) => lineChartX(+d.key))
+            .y((d) => lineChartY(+d.value))(aggregatedDataByTime)
+        );
+    aggregateDataForMap();
+    updateMap();
+}
